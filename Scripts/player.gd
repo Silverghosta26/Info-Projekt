0e2e9503051211e3056e1a0bf1532625bbaebc09
Silverghosta26 <worbsjonathan@gmail.com>
2026-04-25 22:49:04 +0200

    update game mechanics and new elements
    25.04.2026 work with fabi adding new elements like label as timer and new elemnts in player logic

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,12 +4,14 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
 	# Handle jump.
 	if Input.is_action_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -17,9 +19,29 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+
+
+	# flip sprite depending on direction
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+		
+	if direction < 0:
+		animated_sprite_2d.flip_h = true
+
+
+	# play animations
+	if is_on_floor():
+		if direction ==0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+
+
+	# movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
