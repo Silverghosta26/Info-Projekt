4424ab7501f3d97445342a6d53c8d8c49e6d11b6
Blackghosta26 <fabianwald10@gmail.com>
2026-04-25 20:44:15 +0200

    Update 25-04.3036
    Kleine änderungen arbeit mit Jonathan, im movement sowie der Hitbox.

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
-@onready var parcourspawn: Marker2D = %parcourspawn
 
 
 
@@ -9,7 +8,7 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if self.name == "parcour":
 		print("reset parcour")
-		body.global_position = parcourspawn.global_position
+		#body.global_position = parcourspawn.global_position
 	else: timer.start()
 
 
