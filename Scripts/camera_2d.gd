a29c7da211fe6c25d8efe41bdbffb5262bed42f8
Silverghosta26 <worbsjonathan@gmail.com>
2026-04-28 14:48:39 +0200

    ice detector 1 part
    eis detektor in player skript gebaut der tiles unter player checkt und in tilemaplayer nachfragt welche  das ist
    noch kein output außer nachricht im consol gebaut

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -1,7 +1,6 @@
 extends Camera2D
 
 @onready var player: CharacterBody2D = $".."
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("zoom") and player.velocity.length() == 0:
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,15 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var tilemap = get_tree().current_scene.find_child("tiles", true, false)
+
+# eingabe für ice tiles
+var ice_tiles = [
+	Vector2i(6, 0),
+	Vector2i(7, 0),
+	Vector2i(7, 1),
+	Vector2i(6,2)
+]
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -44,3 +53,41 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
+	
+	check_tile_below()
+	
+func check_tile_below():
+	var left = global_position + Vector2(-7, 16)
+	var right = global_position + Vector2(7, 16)
+
+	var cell_left = tilemap.local_to_map(tilemap.to_local(left))
+	var cell_right = tilemap.local_to_map(tilemap.to_local(right))
+
+	var tile_left = tilemap.get_cell_source_id(cell_left)
+	var tile_right = tilemap.get_cell_source_id(cell_right)
+
+	if tile_left != -1 or tile_right != -1:
+		#print("Auf Boden")
+		pass
+		# 👉 Infos holen (linke Seite als Beispiel)
+	if tile_left != -1:
+		var atlas = tilemap.get_cell_atlas_coords(cell_left)
+		handle_tile(tile_left, atlas)
+
+	if tile_right != -1:
+		var atlas = tilemap.get_cell_atlas_coords(cell_right)
+		handle_tile(tile_right, atlas)
+
+	else:
+		print("In der Luft")
+
+
+func handle_tile(tile_id, atlas_coords):
+	# Beispiel: Lava Tile
+	if atlas_coords in ice_tiles:
+		print("Spieler steht auf Eis")
+		
+
+	# Beispiel: Gras Tile
+	elif atlas_coords == Vector2i(0, 0):
+		print("Gras")
