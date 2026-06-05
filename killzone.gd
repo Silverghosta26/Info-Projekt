23bf9f2559fc36dba6058f8e73efde442a266f97
CapXT <coco.der.dud@gmail.com>
2026-06-05 21:19:42 +0200

    upload

---
+++ killzone.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if self.name == "parcour":
+		print("reset parcour")
+		#body.global_position = parcourspawn.global_position
+	else: timer.start()
+
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
